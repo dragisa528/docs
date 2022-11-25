43b60fc0d49070fc36faf2e2b7126d341589e6f2
techking5287 <techking5287@gmail.com>
2022-11-25 15:53:38 +0000

    Skeleton of Docosaurus setup

---
+++ docs/pulsar/index.md
@@ -0,0 +1,75 @@
+---
+sidebar_position: 1
+---
+
+# Pulsar
+
+<p>
+  <strong>WordPress starter theme with Tailwind CSS, Alpine JS and a modern development workflow</strong>
+</p>
+
+## Requirements
+
+Make sure all dependencies have been installed before moving on:
+
+- [WordPress](https://wordpress.org/) >= 5.9
+- [PHP](https://secure.php.net/manual/en/install.php) >= 7.4.0
+- [Node.js](http://nodejs.org/) >= 16
+
+## Theme structure
+
+```sh
+themes/your-theme-name/     # → Root of your theme
+├── blocks/                 # → Custom blocks
+├── config/                 # → Theme configuration files
+│   ├── bindings.php        # → Theme container bindings
+│   ├── block-patterns.php  # → Custom block patterns
+│   ├── block-styles.php    # → Custom block styles
+│   ├── blocks.php          # → All enabled blocks
+│   └── browsersync.json    # → Developer-specific configuration for BrowserSync (if wanted)
+│   └── mix.json            # → Mix entries and other settings
+├── includes/               # → Theme functions and classes
+│   └── classes/            # → Theme classes (autoloaded)
+│       ├── Contracts/      # → Interfaces and Traits
+│       ├── Editor/         # → Editor specific classes
+│       ├── Blocks.php      # → Registration of custom blocks
+│       ├── Patterns.php    # → Block pattern management
+│       ├── Styles.php      # → Block styles management
+│       └── Tools/          # → Various tools used throughout the theme
+│           ├── Mix.php     # → Handles asset management with Laravel Mix
+│           └── Svg.php     # → Allows manipulating SVGs and inlining them
+│       ├── Enqueue.php     # → Theme assets
+│       └── Setup.php       # → Theme setup
+│   └── template-tags/      # → Theme template tags
+│   ├── autoload.php        # → Theme autoloader
+│   ├── compat.php          # → Theme compatibility
+│   └── theme.php           # → Theme mini container
+├── dist/                   # → Built theme assets (never edit)
+├── parts/                  # → Theme partial template files
+├── patterns/               # → Theme block pattern template files
+├── node_modules/           # → Node.js packages (never edit)
+├── src/                    # → Theme assets and templates
+│   ├── css/                # → Theme stylesheets
+│   ├── fonts/              # → Theme fonts
+│   ├── img/                # → Theme images
+│   ├── js/                 # → Theme javascript
+│   ├── svg/                # → Theme SVGs
+├── templates/              # → Theme custom template files
+├── functions.php           # → Theme bootloader
+├── package.json            # → Node.js dependencies and scripts
+├── screenshot.png          # → Theme screenshot for WP admin
+├── style.css               # → Theme meta information
+```
+
+## Theme setup
+
+Edit `app/Setup.php` to enable or disable theme features, setup navigation menus, post thumbnail sizes, and sidebars.
+
+## Theme development
+
+- Run `npm install` from the theme directory to install dependencies
+
+### Build commands
+
+- `npm run watch` — Compile assets when file changes are made, start Browsersync session
+- `npm run build` — Compile assets for production
---
+++ docusaurus.config.js
@@ -0,0 +1,114 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Developer Docs',
+  tagline: 'Documentation for eighteen73 developer packages',
+  url: 'https://docs.eighteen73.co.uk',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'eighteen73', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/eighteen73/docs/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Developer Docs',
+        logo: {
+          alt: 'eighteen73',
+          src: 'img/eighteen73-logo.svg',
+        },
+        items: [
+            {
+              type: 'doc',
+              docId: 'nebula/index',
+              position: 'left',
+              label: 'Nebula',
+            },
+          {
+            type: 'doc',
+            docId: 'pulsar/index',
+            position: 'left',
+            label: 'Pulsar',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} eighteen73`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
