de6c157ceff8d0f98d69d0e4ca2fc3b75faa5438
techking5287 <techking5287@gmail.com>
2022-12-01 15:38:38 +0000

    Fix link causing build fail

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/nebula">
+            to="/wordpress-base/nebula">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
